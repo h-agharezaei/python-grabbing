ea4a5895b579d03cc8a4dad5513041a7797a3bf0
Hossein Agharezaei <h.agharezaei@gmail.com>
2020-11-10 00:17:45 +0330

    Update app.py
    edit object

--- app.py
+++ app.py
@@ -4,31 +4,35 @@ import time
 from urllib.request import urlopen
 
 class sects:  
-    def __init__(self,id, rank, title, h3,body):  
+    def __init__(self,id, rank, type,body):  
         self.id = id  
         self.rank = rank  
-        self.title = title  
-        self.h3 = h3 
+        self.type = type  
         self.body = body
    
 # creating list        
 list = []
 
-counter = 10
+counter = 1
 url = 'http://classic.austlii.edu.au/au/legis/cth/consol_act/ma1958118'
 
 html = urlopen(url).read()
 soup = bs4.BeautifulSoup(html ,"lxml")
 for link in soup.find_all('a', href=True ):
     #link.text
+    counter += 10
     href = link['href']
     if(re.match(r'^s\d*\w+(.html)$',href, re.M|re.I)):
         #print(href)
         html1 = urlopen(url+'/'+href).read()
         soup1 = bs4.BeautifulSoup(html1 ,"lxml")
-        title = soup1.find('b' ).text;
+        title = '<h2>' + soup1.find('b' ).text +'</h2>';
+        list.append( sects(href,counter,'title', title) ) 
+        counter += 1
         #print(title)
-        h3 = soup1.find('h3' ).text;
+        h3 =  '<span><strong>' + soup1.find('h3' ).text +'</strong><span>';
+        list.append( sects(href,counter,'b', h3) ) 
+        counter += 1
         #print(h3)
         body = ''
         for p in soup1.find_all('p'):
@@ -36,10 +40,10 @@ for link in soup.find_all('a', href=True ):
             for link1 in p.find_all('a', href=True):
                 body1 = body1.replace(link1.text, '<a href="'+link1['href']+'">'+ link1.text + '</a>')
             body += body1
-        list.append( sects(href,counter,title,h3, body) ) 
+        list.append( sects(href,counter,'body', body) ) 
         counter += 1
         #print('\n---------------------------------\n')
-        time.sleep(1 )
+        #time.sleep(1 )
         
 for obj in list: 
-    print( obj.id, obj.rank,obj.title,obj.h3,obj.body, sep =' ' )
+    print( obj.id, obj.rank,obj.type, obj.body, sep ='      \n' )
